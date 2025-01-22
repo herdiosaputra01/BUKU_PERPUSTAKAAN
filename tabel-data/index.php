a207b542dc11c05c958bdebb56ef73482afe58ca
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-01-22 11:36:54 +0700

    Bembuat indek data buku

--- tabel-data/index.php
+++ tabel-data/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>Data Buku</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/all.css">
 </head>
@@ -17,85 +17,45 @@
         <div class="col-12 m-auto">
             <div class="card">
             <div class="card-header">
-                <h3 class="float-start">Data Mahasiswa</h3>
-                <span class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i> Tambah Data</a></span>
+                <h3 class="float-start">Data Buku</h3>
+                <span class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i>Tambah Data</a></span>
             </div>
             <div class="card-body">
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">NIM</th>
-                        <th scope="col">Nama Mahasiswa</th>
-                        <th scope="col">Jenis Kelamin</th>
-                        <th scope="col">Jurusan</th>
-                        <th scope="col">Dosen Wali</th>
+                        <th scope="col">No</th>
+                        <th scope="col">Id buku</th>
+                        <th scope="col">Judul Buku</th>
+                        <th scope="col">Penulis</th>
+                        <th scope="col">Id Katagori</th>
+                        <th scope="col">Cover Buku</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                    #1. koneksikan file ini
-                    include("../koneksi.php");
+                <?php
+                                #1. koneksikan file ini
+                                include("../koneksi.php");
 
-                    #2. menulis query
-                    $tampil = "SELECT *, mahasiswas.nama as nm_mhs, dosens.nama as nm_dos 
-                    FROM mahasiswas 
-                    INNER JOIN jurusans ON mahasiswas.jurusans_id=jurusans.id
-                    INNER JOIN dosens ON mahasiswas.dosens_id=dosens.id";
+                                #2. menulis query
+                                $tampil = "SELECT * FROM bukus";
 
-                    #3. jalankan query
-                    $proses = mysqli_query($koneksi, $tampil);
+                                #3. jalankan query
+                                $proses = mysqli_query($koneksi, $tampil);
 
-                    #4. looping data dari database
-                    $nomor = 1;
-                    foreach($proses as $data){
-                    ?>
+                                #4. looping data dari database
+                                $nomor = 1;
+                                foreach ($proses as $data) {
+                                ?>
                     <tr>
                         <th scope="row"><?=$nomor++?></th>
-                        <td><?=$data['nim']?></td>
-                        <td><?=$data['nm_mhs']?></td>
-                        <td><?=$data['jk']?></td>
-                        <td><?=$data['jurusan']?></td>
-                        <td><?=$data['nm_dos']?></td>
+                        <td><?=$data['id_buku']?></td>
+                        <td><?=$data['judul_buku']?></td>
+                        <td><?=$data['penulis']?></td>
+                        <td><?=$data['id_katagori']?></td>
+                        <td><?=$data['cover_buku']?></td>
                         <td>
-                            <!-- TOMBOL DETAIL -->
-                            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#detail<?=$data['nim']?>">
-                            <i class="fa-solid fa-eye"></i>
-                            </button>
-
-                            <!-- MODAL DETAIL-->
-                            <div class="modal fade" id="detail<?=$data['nim']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Data <?=$data['nm_mhs']?> </h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <img width="200" src="foto/<?=$data['foto']?>" alt="">
-                                    <table class="table">
-                                        <tr>
-                                            <td scope="col">NIM</td>
-                                            <th scope="col">: <?=$data['nim']?></th>
-                                        </tr>
-                                        <tr>
-                                            <td scope="col">Nama Lengkap</td>
-                                            <th scope="col">: <?=$data['nm_mhs']?></th>
-                                        </tr>
-                                        <tr>
-                                            <td scope="col">Tempat Lahir</td>
-                                            <th scope="col">: <?=$data['tmp_lahir']?></th>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                                    
-                                </div>
-                                </div>
-                            </div>
-                            </div>
 
                             <!-- TOMBOL EDIT -->
                             <a class="btn btn-info btn-sm" href="edit.php?id=<?=$data['id']?>"><i class="fa fa-pen-to-square"></i></a>
